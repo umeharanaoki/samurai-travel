832e41d8f45c77ee6983c537b93ad0a8633daebd
N.umehara <n6773938@gmail.com>
2023-11-03 10:07:50 +0900

    up to chapter18

--- samuraitravel/src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
@@ -8,22 +8,32 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.samuraitravel.entity.House;
+import com.example.samuraitravel.form.HouseEditForm;
 import com.example.samuraitravel.form.HouseRegisterForm;
 import com.example.samuraitravel.repository.HouseRepository;
+import com.example.samuraitravel.service.HouseService;
 
 @Controller
 @RequestMapping("/admin/houses")
 public class AdminHouseController {
 	private final HouseRepository houseRepository;
+	private final HouseService houseService;
 	
-	public AdminHouseController(HouseRepository houseRepository) {
+	public AdminHouseController(HouseRepository houseRepository, HouseService houseService) {
 		this.houseRepository = houseRepository;
+		this.houseService = houseService;
+		
 	}
 	
 	@GetMapping
@@ -55,5 +65,52 @@ public class AdminHouseController {
 	public String register(Model model) {
 		model.addAttribute("houseRegisterForm", new HouseRegisterForm());
 		return "admin/houses/register";
+	}
+	
+	@PostMapping("/create")
+	// ModelAttributeでフォームから送られてきたデータの割り当て、Validatedでデータの形式等に問題がないか確認
+	public String create(@ModelAttribute @Validated HouseRegisterForm houseRegisterForm, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+		if(bindingResult.hasErrors()) {
+			// hasErrorsメソッドでエラーを確認し、ある場合民宿登録ページを表示
+			return "admin/houses/register";
+		}
+		// エラーがない場合、createメソッドで民宿を登録
+		houseService.create(houseRegisterForm);
+		// リダイレクト先にデータを渡す
+		redirectAttributes.addFlashAttribute("successMessage", "民宿を登録しました。"); // 第1引数：リダイレクト先から参照する変数名　第2引数：リダイレクト先に渡すデータ
+		// 民宿の登録に成功した場合民宿一覧ページにリダイレクトする
+		return "redirect:/admin/houses";
+	}
+	
+	@GetMapping("/{id}/edit")
+	public String edit(@PathVariable(name = "id") Integer id, Model model) {
+		House house = houseRepository.getReferenceById(id);
+		String imageName = house.getImageName();
+		HouseEditForm houseEditForm = new HouseEditForm(house.getId(), house.getName(), null, house.getDescription(), house.getPrice(), house.getCapacity(), house.getPostalCode(), house.getAddress(), house.getPhoneNumber());
+		
+		model.addAttribute("imageName", imageName);
+		model.addAttribute("houseEditForm", houseEditForm);
+		return "admin/houses/Edit";
+	}
+	
+	@PostMapping("/{id}/update")
+	public String update(@ModelAttribute @Validated HouseEditForm houseEditForm, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+		if(bindingResult.hasErrors()) {
+			return "admin/houses/edit";
+		}
+		
+		houseService.update(houseEditForm);
+		redirectAttributes.addFlashAttribute("successMessage", "民宿情報を編集しました。");
+		
+		return "redirect:/admin/houses";
+	}
+	
+	@PostMapping("/{id}/delete")
+	public String delete(@PathVariable(name = "id") Integer id, RedirectAttributes redirectAttributes) {
+		houseRepository.deleteById(id);
+		
+		redirectAttributes.addFlashAttribute("successMessage", "民宿を削除しました。");
+		
+		return "redirect:/admin/houses";
 	}
 }
--- samuraitravel/src/main/java/com/example/samuraitravel/service/HouseService.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/service/HouseService.java
@@ -1,5 +1,104 @@
 package com.example.samuraitravel.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+// java.ioパッケージはデータの入出力関連
+// java.nioパッケージはデータのコンテナ（バッファ）関連
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.samuraitravel.entity.House;
+import com.example.samuraitravel.form.HouseEditForm;
+import com.example.samuraitravel.form.HouseRegisterForm;
+import com.example.samuraitravel.repository.HouseRepository;
+
+// Serviceはビジネスロジックを担当
+@Service
 public class HouseService {
+	
+	private final HouseRepository houseRepository;
+	
+	public HouseService(HouseRepository houseRepository) {
+		this.houseRepository = houseRepository;
+	}
+	
+	@Transactional
+	public void create(HouseRegisterForm houseRegisterForm) {
+		House house = new House();
+		// 一時的に画像ファイルを保存
+		MultipartFile imageFile = houseRegisterForm.getImageFile();
+		// 画像ファイルがある場合（画像アップロードされたとき）
+		// 画像の名前やパスを定義して登録された画像を格納＆表示できるようにする
+		if(!imageFile.isEmpty()) {
+			String imageName = imageFile.getOriginalFilename();
+			// メソッドは下で定義(hashedはある一定のルールで変換されたの意）
+			String hashedImageName = generateNewFileName(imageName);
+			Path filePath = Paths.get("src/main/resouces/static/storage/" + hashedImageName);
+			copyImageFile(imageFile, filePath);
+			house.setImageName(hashedImageName);
+		}
+		
+		// フォームに入力された内容をセット
+		// setterなくない？→Controllerに
+		house.setName(houseRegisterForm.getName());
+		house.setDescription(houseRegisterForm.getDescription());
+		house.setPrice(houseRegisterForm.getPrice());
+		house.setCapacity(houseRegisterForm.getCapacity());
+		house.setPostalCode(houseRegisterForm.getPostalCode());
+		house.setAddress(houseRegisterForm.getAddress());
+		house.setPhoneNumber(houseRegisterForm.getPhoneNumber());
+		// エンティティをデータベースに保存
+		houseRepository.save(house);
+	}
+	
+	// 民宿の更新機能
+	@Transactional
+	public void update(HouseEditForm houseEditForm) {
+		House house = houseRepository.getReferenceById(houseEditForm.getId());
+		MultipartFile imageFile = houseEditForm.getImageFile();
+		
+		if(!imageFile.isEmpty()) {
+			String imageName = imageFile.getOriginalFilename();
+			String hashedImageName = generateNewFileName(imageName);
+			Path filePath = Paths.get("src/main/resources/static/storage/" + hashedImageName);
+			copyImageFile(imageFile, filePath);
+			house.setImageName(hashedImageName);
+		}
+		
+		house.setName(houseEditForm.getName());
+		house.setDescription(houseEditForm.getDescription());
+		house.setPrice(houseEditForm.getPrice());
+		house.setCapacity(houseEditForm.getCapacity());
+		house.setPostalCode(houseEditForm.getPostalCode());
+		house.setAddress(houseEditForm.getAddress());
+		house.setPhoneNumber(houseEditForm.getPhoneNumber());
+		
+		houseRepository.save(house);
+	}
+	
+	// UUIDを使って生成したファイル名を返す
+	public String generateNewFileName(String fileName) {
+		String[] fileNames = fileName.split("\\.");
+		for(int i = 0; i < fileNames.length - 1; i++) {
+			fileNames[i] = UUID.randomUUID().toString();
+		}
+		String hashedFileName = String.join(".", fileNames);
+		return hashedFileName;	
+	}
+	
+	// 画像ファイルを指定したファイルにコピーする
+	public void copyImageFile(MultipartFile imageFile, Path filePath) {
+		try {
+			Files.copy(imageFile.getInputStream(), filePath);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 
 }
