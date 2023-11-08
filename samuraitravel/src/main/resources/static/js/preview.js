24e55e60d850e6e0faa721c1072c5a5733e2c2ba
N.umehara <n6773938@gmail.com>
2023-11-08 10:43:04 +0900

    add supplement, to find errors

--- samuraitravel/src/main/resources/static/js/preview.js
+++ samuraitravel/src/main/resources/static/js/preview.js
@@ -2,7 +2,7 @@ const imageInput = document.getElementById('imageFile');
 const imagePreview = document.getElementById('imagePreview');
 
 imageInput.addEventListener('change', () => {
-	if (imageInput.files[0]) {
+	if (imageInput.files[0]){
 		let fileReader = new FileReader();
 		fileReader.onload = () => {
 			imagePreview.innerHTML = `<img src="${fileReader.result}" class="mb-3">`;
